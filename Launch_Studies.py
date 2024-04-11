c1deb58df672fb61ba584c39adc401aa415c327f
dansoyeux <dan.soyeux.1@etsmtl.net>
2024-04-11 13:05:24 -0400

    h5 cas extrèmes

--- Launch_Studies.py
+++ Launch_Studies.py
@@ -25,41 +25,46 @@ MovementType_list = ["CoronalElevation", "ScapularElevation", "SagitalElevation"
 
 # %% Cas de simulation
 
-glenoid_lateralisation_offset_list = [-10, -5, 0, 5]
-glenoid_superior_offset_list = [-6, -4, -2, 0]
-humeral_lateralisation_list = [0, 5, 10, 15]
+# glenoid_lateralisation_offset_list = [-10, -5, 0, 5]
+# glenoid_superior_offset_list = [-6, -4, -2, 0]
+# glenoid_lateral_offset = [0, 5, 10, 15]
+
+glenoid_lateralisation_offset_list = [-10, 5]
+glenoid_superior_offset_list = [-6, 0]
+glenoid_lateral_offset_list = [0, 15]
 
 # %% Script lancement simulation
 
 macrolist = []
 
 for MovementType in MovementType_list:
+    for humeral_lateral_offset in glenoid_lateral_offset_list:
+        for glenoid_lateral_offset in glenoid_lateralisation_offset_list:
 
-    for glenoid_medial_offset in glenoid_medial_offset_list:
+            for glenoid_superior_offset in glenoid_superior_offset_list:
 
-        for glenoid_superior_offset in glenoid_superior_offset_list:
+                ResultFileName = f"{MovementType}_humLat_{humeral_lateral_offset}_glenLat_{glenoid_lateral_offset}_glenSup_{glenoid_superior_offset}"
 
-            ResultFileName = f"{MovementType}_IS_{glenoid_superior_offset}_ML_{glenoid_medial_offset}"
-
-            macrolist.append([
-                Load('rTSA-Lateralisation.main.any',
-                     defs={'MovementType': f"{MovementType}",
-                           'ResultFileName': f'"{ResultFileName}"',
-                           'glenoid_anteriorisation': 0,
-                           'glenoid_distalisation': glenoid_superior_offset,
-                           'glenoid_medialisation': glenoid_medial_offset,
-                           'humeral_lateralisation': glenoid_medial_offset,
-                           'rTSA': 1,
-                           'GHReactionsOn': 0,
-                           'AutoSaveOption': 1,
-                           },  # fin defs
-                     ),  # fin Load
-                OperationRun('Main.RunApplication')
-            ])
-
+                macrolist.append([
+                    Load('rTSA-Lateralisation.main.any',
+                         defs={'MovementType': f"{MovementType}",
+                               'ResultFileName': f'"{ResultFileName}"',
+                               'glenoid_anteriorisation': 0,
+                               'glenoid_distalisation': glenoid_superior_offset,
+                               'glenoid_lateralisation': glenoid_lateral_offset,
+                               'humeral_lateralisation': humeral_lateral_offset,
+                               'rTSA': 1,
+                               'GHReactionsOn': 0,
+                               'AutoSaveOption': 1,
+                               },  # fin defs
+                         ),  # fin Load
+                    OperationRun('Main.RunApplication')
+                ])
 
 # %% Launch study without timeout
 
 app = AnyPyProcess(timeout=3600 * 100, num_processes=num_processes,
-                   anybodycon_path=r"C:\Users\user\AppData\Local\Programs\AnyBody Technology\AnyBody.8.0\anybodycon.exe")
+                   # uncomment if on local pc
+                   # anybodycon_path=r"C:\Users\user\AppData\Local\Programs\AnyBody Technology\AnyBody.8.0\anybodycon.exe"
+                   )
 app.start_macro(macrolist)
